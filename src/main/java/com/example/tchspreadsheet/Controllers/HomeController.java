a4045763e4c8534331b8fa1bd42474a66c2fe693
Nick Adams <nick.pappas.adams@gmail.com>
2022-02-27 15:32:46 -0600

    added functioning login page with redirect to homepage

---
+++ src/main/java/com/example/tchspreadsheet/Controllers/HomeController.java
@@ -0,0 +1,30 @@
+package com.example.tchspreadsheet.Controllers;
+
+
+import com.example.tchspreadsheet.Models.User;
+import com.example.tchspreadsheet.Repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
+
+
+@Controller
+public class HomeController {
+
+    private final UserRepository userDao;
+
+    public HomeController(UserRepository userDao) {
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        User loginUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Optional<User> user = userDao.findById(loginUser.getId());
+        model.addAttribute("homepage", user.get());
+        return "users/home";
+    }
+}
--- src/main/java/com/example/tchspreadsheet/Models/User.java
+++ src/main/java/com/example/tchspreadsheet/Models/User.java
@@ -20,7 +20,7 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column
+    @Column(columnDefinition = "TINYINT(1)")
     private boolean isAdmin;
 
     // authentication copy constructor
