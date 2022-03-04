851a60e627394fd8cf5b2303f18189f136dbc02e
Nick Adams <nick.pappas.adams@gmail.com>
2022-03-04 14:37:55 -0600

    added variables for twos and fives totals

--- src/main/resources/public/js/count.js
+++ src/main/resources/public/js/count.js
@@ -18,14 +18,25 @@ submitButton.addEventListener("click", (e) => {
     let loseOnes = document.getElementById("loseOnes").value;
     let strappedOnes = document.getElementById("strappedOnes").value;
     let loseTwos = document.getElementById("loseTwos").value;
+    let strappedTwos = document.getElementById("strappedTwos").value;
+    let loseFives = document.getElementById("loseFives").value;
+    let strappedFives = document.getElementById("strappedFives").value;
+    let loseTens = document.getElementById("loseTens").value;
+    let strappedTens = document.getElementById("strappedTens").value;
+    let loseTwenties = document.getElementById("loseTwenties");
+    let strappedTwenties = document.getElementById("strappedTwenties").value;
+    let loseHundreds = document.getElementById("loseHundreds").value;
+    let strappedHundreds = document.getElementById("strappedHundreds").value;
     let userTotals = document.getElementById("computedTotal");
     console.log(loseTwos);
 
     const loseOnesTotal = totals(parseInt(loseOnes), 1);
     const strappedOnesTotal = totals(parseInt(strappedOnes), 100);
     const loseTwosTotal = totals(parseInt(loseTwos), 2);
-    const computedTotal = grandTotal(loseOnesTotal, strappedOnesTotal,loseTwosTotal);
-
+    const strappedTwosTotal = totals(parseInt(strappedTwos), 200);
+    const loseFivesTotal = totals(parseInt(loseFives), 5);
+    const strappedFivesTotal = totals(parseInt(strappedFives), 500);
+    const computedTotal = grandTotal(loseOnesTotal, strappedOnesTotal, loseTwosTotal);
 
     userTotals.innerHTML = ("$" + computedTotal);
 
