340822af5579642e6fc68d1df5c78ea1f6dd3637
Nick Adams <nick.pappas.adams@gmail.com>
2022-03-04 14:26:45 -0600

    added userTotal variable

--- src/main/resources/public/js/count.js
+++ src/main/resources/public/js/count.js
@@ -8,15 +8,27 @@ const totals = (input, multiplier) => {
     return input * multiplier;
 }
 
+const grandTotal = (a, b, c) => {
+    return a + b + c;
+}
+
 
 submitButton.addEventListener("click", (e) => {
     e.preventDefault();
     let loseOnes = document.getElementById("loseOnes").value;
     let strappedOnes = document.getElementById("strappedOnes").value;
+    let loseTwos = document.getElementById("loseTwos").value;
+    let userTotals = document.getElementById("computedTotal");
+    console.log(loseTwos);
 
-    const loseOnesTotal = totals(loseOnes, 1);
-    const strappedOnesTotal = totals(strappedOnes, 100);
-    computedTotal.innerHTML = loseOnesTotal + strappedOnesTotal;
-})
+    const loseOnesTotal = totals(parseInt(loseOnes), 1);
+    const strappedOnesTotal = totals(parseInt(strappedOnes), 100);
+    const loseTwosTotal = totals(parseInt(loseTwos), 2);
+    const computedTotal = grandTotal(loseOnesTotal, strappedOnesTotal,loseTwosTotal);
+
+
+    userTotals.innerHTML = ("$" + computedTotal);
+
+});
 
 
