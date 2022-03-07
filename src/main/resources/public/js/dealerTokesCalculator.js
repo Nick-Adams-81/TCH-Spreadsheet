247535a81f15f78482b8a1f7f8e2d2fd48cffd3b
Nick Adams <nick.pappas.adams@gmail.com>
2022-03-07 13:49:44 -0600

    added tokes calculator

--- src/main/resources/public/js/dealerTokesCalculator.js
+++ src/main/resources/public/js/dealerTokesCalculator.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log("test")
+
+const button = document.getElementById("calculate-percent");
+
+const calculator = (input) => {
+    return parseInt(input) * .88;
+}
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    const totalTokes = document.getElementById("totalTokes").value;
+    const dealerTakeHomeTotal = document.getElementById("dealerTakeHomeTotal");
+    const twelvePercent = document.getElementById("twelvePercent");
+    dealerTakeHomeTotal.value = Math.floor(calculator(totalTokes));
+    twelvePercent.value = totalTokes - Math.floor(calculator(totalTokes));
+
+})
+
