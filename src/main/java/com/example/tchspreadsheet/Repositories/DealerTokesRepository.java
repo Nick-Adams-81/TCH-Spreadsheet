ff0beb89cd201c5784e71d83681a9db39148a276
Nick Adams <nick.pappas.adams@gmail.com>
2022-03-06 19:13:22 -0600

    added repository for dealer tokes

---
+++ src/main/java/com/example/tchspreadsheet/Repositories/DealerTokesRepository.java
@@ -0,0 +1,7 @@
+package com.example.tchspreadsheet.Repositories;
+
+import com.example.tchspreadsheet.Models.DealerTokes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DealerTokesRepository extends JpaRepository<DealerTokes, Long> {
+}
