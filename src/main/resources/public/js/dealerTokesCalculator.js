7e453aa241410b456b0727c7592bf82fd04ab2ba
Nick Adams <nick.pappas.adams@gmail.com>
2022-03-08 14:28:19 -0600

    added notes to js file

--- src/main/resources/public/js/dealerTokesCalculator.js
+++ src/main/resources/public/js/dealerTokesCalculator.js
@@ -1,12 +1,14 @@
 "use strict";
 
-
+// getting the button id
 const button = document.getElementById("calculate-percent");
 
+// function to calculate 12%
 const calculator = (input) => {
     return parseInt(input) * .88;
 }
 
+// adding the event listener to calculate the 12% on button click
 button.addEventListener("click", (e) => {
     e.preventDefault();
     const totalTokes = document.getElementById("totalTokes").value;
