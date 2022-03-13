74590c2876c69320355c3248c4b0d80fbcfa918e
Nick Adams <nick.pappas.adams@gmail.com>
2022-03-13 14:11:59 -0500

    added start shift button click event test

---
+++ src/main/resources/public/js/dateAndTime.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const startShiftButton = document.getElementById("startShiftButton");
+const pauseShiftButton = document.getElementById("pauseShiftButton");
+const endShiftButton = document.getElementById("endShiftButton");
+
+
+startShiftButton.addEventListener("click", () => {
+    console.log("test click event")
+});
