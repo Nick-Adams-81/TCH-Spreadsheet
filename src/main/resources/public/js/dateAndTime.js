b11be7def2fc1bdf7fa84372a81a229704a44826
Nick Adams <nick.pappas.adams@gmail.com>
2022-03-13 15:10:31 -0500

    added time with am and pm values

--- src/main/resources/public/js/dateAndTime.js
+++ src/main/resources/public/js/dateAndTime.js
@@ -4,19 +4,40 @@ const startShiftButton = document.getElementById("startShiftButton");
 const pauseShiftButton = document.getElementById("pauseShiftButton");
 const endShiftButton = document.getElementById("endShiftButton");
 
-
+// setting up a new date variable //
 const today = new Date();
-const date =  (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
 
+// getting the current date //
+const date = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
 
+//function to convert military time to standard time
+const standardTime = (hours) => {
+    if(hours > 12) return hours - 12;
+     else return hours;
+}
+
+// getting the current time //
+
+// getting current military time in hours
+const timeHours = today.getHours();
+
+// getting current minutes in hours
+const timeMinutes = today.getMinutes();
+
+// function to add a 0 if minutes is less than 10
+const minutes = (mins) => {
+    if(mins < 10) return "0" + mins;
+    else return mins;
+}
 
 
 startShiftButton.addEventListener("click", () => {
-    console.log(date)
+    console.log(date);
 });
 
 pauseShiftButton.addEventListener("click", () => {
-    console.log("test pause shift click event")
+    if(timeHours > 12) console.log(standardTime(timeHours) + ":" + minutes(timeMinutes) + " pm");
+    else console.log(timeHours + ":" + minutes(timeMinutes) + " am")
 });
 
 endShiftButton.addEventListener("click", () => {
