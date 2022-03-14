16707019f371f2a31d79e76fe98f7965da04be80
Nick Adams <nick.pappas.adams@gmail.com>
2022-03-14 15:35:48 -0500

    logging typeof time variables

--- src/main/resources/public/js/dateAndTime.js
+++ src/main/resources/public/js/dateAndTime.js
@@ -11,21 +11,22 @@ const today = new Date();
 // getting the current date //
 const date = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
 console.log(typeof date);
+
+
+
+// getting the current time //
+
+// getting current military(24-hour clock) time in hours
+const timeHours = today.getHours();
+
+// getting current minutes
+const timeMinutes = today.getMinutes();
+
 //function to convert military time to standard time
 const standardTime = (hours) => {
     if(hours > 12) return hours - 12;
     else return hours;
 }
-
-// getting the current time //
-
-// getting current military time in hours
-const timeHours = today.getHours();
-
-// getting current minutes in hours
-const timeMinutes = today.getMinutes();
-
-
 
 // function to add a 0 if minutes is less than 10
 const minutes = (mins) => {
@@ -33,14 +34,18 @@ const minutes = (mins) => {
     else return mins;
 }
 
+// converting 0 hundred hours to 12 midnight
 const midnightConversion = (hours) => {
     if(hours === 0) return hours + 12;
     else return hours;
  }
 
+ // creating variables to hold date and time
 let timeAm = `${date} at: ${standardTime(timeHours)}:${minutes(timeMinutes)} am`;
 let timePm = `${date} at: ${standardTime(timeHours)}:${minutes(timeMinutes)} pm`;
-let midnight = `${date} at: ${standardTime(midnightConversion(timeHours))}:${minutes(timeMinutes)} am`;
+let midnight = `${date} at: ${midnightConversion(timeHours)}:${minutes(timeMinutes)} am`;
+
+console.log(typeof timeAm);
 
 
 
