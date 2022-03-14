ab35dec48870a5a648cf61d7c9e3d977942ae14e
Nick Adams <nick.pappas.adams@gmail.com>
2022-03-14 15:30:47 -0500

    sused out variables and functions for getting current time and date

--- src/main/resources/public/js/dateAndTime.js
+++ src/main/resources/public/js/dateAndTime.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// getting buttons from dom by id //
 const startShiftButton = document.getElementById("startShiftButton");
 const pauseShiftButton = document.getElementById("pauseShiftButton");
 const endShiftButton = document.getElementById("endShiftButton");
@@ -9,11 +10,11 @@ const today = new Date();
 
 // getting the current date //
 const date = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
-
+console.log(typeof date);
 //function to convert military time to standard time
 const standardTime = (hours) => {
     if(hours > 12) return hours - 12;
-     else return hours;
+    else return hours;
 }
 
 // getting the current time //
@@ -24,24 +25,40 @@ const timeHours = today.getHours();
 // getting current minutes in hours
 const timeMinutes = today.getMinutes();
 
+
+
 // function to add a 0 if minutes is less than 10
 const minutes = (mins) => {
     if(mins < 10) return "0" + mins;
     else return mins;
 }
 
+const midnightConversion = (hours) => {
+    if(hours === 0) return hours + 12;
+    else return hours;
+ }
 
+let timeAm = `${date} at: ${standardTime(timeHours)}:${minutes(timeMinutes)} am`;
+let timePm = `${date} at: ${standardTime(timeHours)}:${minutes(timeMinutes)} pm`;
+let midnight = `${date} at: ${standardTime(midnightConversion(timeHours))}:${minutes(timeMinutes)} am`;
+
+
+
+// adding click events to button elements //
 startShiftButton.addEventListener("click", () => {
-    if(timeHours > 12) console.log(date + " at: " + standardTime(timeHours) + ":" + minutes(timeMinutes) + " pm");
-    else console.log(date + " at: " + timeHours + ":" + minutes(timeMinutes) + " am");
+    if(timeHours >= 12) console.log(timePm);
+    else if(timeHours === 0) console.log(midnight);
+    else console.log(timeAm);
 });
 
 pauseShiftButton.addEventListener("click", () => {
-    if(timeHours > 12) console.log(date + " at: " + standardTime(timeHours) + ":" + minutes(timeMinutes) + " pm");
-    else console.log(date + " at: " + timeHours + ":" + minutes(timeMinutes) + " am");
+    if(timeHours >= 12) console.log(timePm);
+    else if(timeHours === 0) console.log(midnight);
+    else console.log(timeAm);
 });
 
 endShiftButton.addEventListener("click", () => {
-    if(timeHours > 12) console.log(date + " at: " + standardTime(timeHours) + ":" + minutes(timeMinutes) + " pm");
-    else console.log(date + " at: " + timeHours + ":" + minutes(timeMinutes) + " am");
-})
+    if(timeHours >= 12) console.log(timePm);
+    else if(timeHours === 0) console.log(midnight);
+    else console.log(timeAm);
+});
