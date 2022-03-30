9df35d02be88d7d43d3e13d9d74f0e3dc621dbff
Nick Adams <nick.pappas.adams@gmail.com>
2022-03-30 12:27:14 -0500

    added jpa repository for dealers

--- src/main/java/com/example/tchspreadsheet/Models/Dealer.java
+++ src/main/java/com/example/tchspreadsheet/Models/Dealer.java
@@ -2,6 +2,7 @@ package com.example.tchspreadsheet.Models;
 
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "dealers")
@@ -16,6 +17,17 @@ public class Dealer {
 
     @Column(length = 4)
     private int pin;
+
+    @ManyToMany(mappedBy = "dealers")
+    private List<User> users;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "dealers_dealer_tokes_bridge",
+            joinColumns = {@JoinColumn(name = "dealer_id")},
+            inverseJoinColumns = {@JoinColumn(name = "dealer_tokes_id")}
+    )
+    private List<DealerTokes> dealerTokes;
 
     public long getId() {
         return id;
@@ -39,5 +51,21 @@ public class Dealer {
 
     public void setPin(int pin) {
         this.pin = pin;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<DealerTokes> getDealerTokes() {
+        return dealerTokes;
+    }
+
+    public void setDealerTokes(List<DealerTokes> dealerTokes) {
+        this.dealerTokes = dealerTokes;
     }
 }
---
+++ src/main/java/com/example/tchspreadsheet/Repositories/DealerRepository.java
@@ -0,0 +1,7 @@
+package com.example.tchspreadsheet.Repositories;
+
+import com.example.tchspreadsheet.Models.Dealer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DealerRepository extends JpaRepository<Dealer, Long> {
+}
