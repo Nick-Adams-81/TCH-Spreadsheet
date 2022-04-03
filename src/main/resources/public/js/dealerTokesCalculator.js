b47a12bd8fa09b9a487b6122385faa1081162896
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-02 20:35:26 -0500

    added todo in dealer tokes js file

--- src/main/resources/public/js/dealerTokesCalculator.js
+++ src/main/resources/public/js/dealerTokesCalculator.js
@@ -1,18 +1,21 @@
 "use strict";
 
-// getting the button id
+// getting elements from html by id
 const button = document.getElementById("calculate-percent");
 const dealersNames = document.getElementById("dealers-names");
 const dealerName = document.getElementById("dealers-name");
 const dealerButton = document.getElementById("dealer-button");
+const todaysDate = document.getElementById("current-date");
+const submitButton = document.getElementById("submit-button");
+const submitForm = document.getElementById("submit-form");
 
+// TODO: make this function auto fill the dealer name in the proper field
 dealerButton.addEventListener("click", (e) => {
     e.preventDefault();
     dealerName.value = dealersNames.value;
 })
 
 // setting up and getting today's date
-const todaysDate = document.getElementById("current-date");
 const today = new Date();
 const date = `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`;
 todaysDate.value = date;
@@ -25,10 +28,14 @@ const calculator = (input) => {
 // adding the event listener to calculate the 12% on button click
 button.addEventListener("click", (e) => {
     e.preventDefault();
-    const totalTokes = document.getElementById("totalTokes").value;
-    const dealerTakeHomeTotal = document.getElementById("dealerTakeHomeTotal");
-    const twelvePercent = document.getElementById("twelvePercent");
+    let totalTokes = document.getElementById("totalTokes").value;
+    let dealerTakeHomeTotal = document.getElementById("dealerTakeHomeTotal");
+    let twelvePercent = document.getElementById("twelvePercent");
     dealerTakeHomeTotal.value = Math.floor(calculator(totalTokes));
     twelvePercent.value = totalTokes - Math.floor(calculator(totalTokes));
 });
+
+// submitButton.addEventListener("click", () => {
+//     setTimeout(submitForm.reset(), 200);
+// });
 
