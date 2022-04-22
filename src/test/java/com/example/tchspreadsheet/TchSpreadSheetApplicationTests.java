b878e1e9948f59a7ee7f82f894c9441a1f120cdf
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-22 15:20:31 -0500

    added first junit tests

--- src/test/java/com/example/tchspreadsheet/TchSpreadSheetApplicationTests.java
+++ src/test/java/com/example/tchspreadsheet/TchSpreadSheetApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.tchspreadsheet;
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.*;
 
 @SpringBootTest
 class TchSpreadSheetApplicationTests {
 
     @Test
     void contextLoads() {
+        String expected = "Codeup";
+        String actual = "Codeup";
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testNull() {
+        String string = null;
+        Assertions.assertNull(string);
     }
 
 }
