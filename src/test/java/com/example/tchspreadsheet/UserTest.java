a0c0e2e12f9feba0298db474851d7fd587f7fbc6
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-26 13:50:02 -0500

    added first user tests

---
+++ src/test/java/com/example/tchspreadsheet/UserTest.java
@@ -0,0 +1,38 @@
+package com.example.tchspreadsheet;
+
+import com.example.tchspreadsheet.Models.User;
+import org.junit.Before;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+@SpringBootTest
+public class UserTest {
+
+    User emptyUser;
+    User userWithOne;
+    User userWithMany;
+
+    @Before
+    public void setupUser() {
+
+        emptyUser = new User();
+
+        userWithOne = new User();
+        userWithOne.setUsername("test user");
+
+        userWithMany = new User();
+        userWithMany.setUsername("test user 2");
+        userWithMany.setPassword("test-password");
+        userWithMany.setEmail("test@test.com");
+    }
+
+    @Test
+    public void testIsEmpty() {
+        assertTrue(emptyUser.getUsername(), true);
+        assertFalse(userWithMany.getUsername(), false);
+    }
+
+
+
+}
