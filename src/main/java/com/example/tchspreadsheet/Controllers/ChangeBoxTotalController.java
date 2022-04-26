f086d10a6d960c13e808879af442a54b67ea3881
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-26 18:16:32 -0500

    added get mapping controller to change box total

--- src/main/java/com/example/tchspreadsheet/Controllers/ChangeBoxTotalController.java
+++ src/main/java/com/example/tchspreadsheet/Controllers/ChangeBoxTotalController.java
@@ -1,8 +1,12 @@
 package com.example.tchspreadsheet.Controllers;
 
 
+import com.example.tchspreadsheet.Models.User;
 import com.example.tchspreadsheet.Repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChangeBoxTotalController {
@@ -14,4 +18,11 @@ public class ChangeBoxTotalController {
     }
 
 
+    @GetMapping("/changeBoxTotal")
+    public String boxTotal(Model model) {
+        User loginUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userDao.findById(loginUser.getId());
+        model.addAttribute("user", user.getId());
+        return "changeBoxTotal";
+    }
 }
