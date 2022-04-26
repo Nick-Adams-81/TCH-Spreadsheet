90627968ba05b2deacaadc6fd4b598ccd00592e4
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-26 18:12:56 -0500

    added constructor to box total controller

--- src/main/java/com/example/tchspreadsheet/Controllers/ChangeBoxTotalController.java
+++ src/main/java/com/example/tchspreadsheet/Controllers/ChangeBoxTotalController.java
@@ -1,8 +1,17 @@
 package com.example.tchspreadsheet.Controllers;
 
 
+import com.example.tchspreadsheet.Repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChangeBoxTotalController {
+
+    private UserRepository userDao;
+
+    public ChangeBoxTotalController(UserRepository userDao) {
+        this.userDao = userDao;
+    }
+
+
 }
