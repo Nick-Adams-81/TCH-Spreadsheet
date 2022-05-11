0019f821e2c376917d068105b674daf4ae20fea9
Nick Adams <nick.pappas.adams@gmail.com>
2022-05-11 10:45:21 -0500

    added post mapping to change box totals

--- src/main/java/com/example/tchspreadsheet/Controllers/ChangeBoxTotalController.java
+++ src/main/java/com/example/tchspreadsheet/Controllers/ChangeBoxTotalController.java
@@ -1,12 +1,15 @@
 package com.example.tchspreadsheet.Controllers;
 
 
+import com.example.tchspreadsheet.Models.Spreadsheet;
 import com.example.tchspreadsheet.Models.User;
 import com.example.tchspreadsheet.Repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ChangeBoxTotalController {
@@ -23,6 +26,13 @@ public class ChangeBoxTotalController {
         User loginUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = userDao.findById(loginUser.getId());
         model.addAttribute("user", user.getId());
+        return "changeBoxTotal";
+    }
+
+    @PostMapping("/changeBoxTotal")
+    public String changeBoxTotal(@ModelAttribute Spreadsheet spreadsheet) {
+        User loginUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userDao.findById(loginUser.getId());
         return "changeBoxTotal";
     }
 }
